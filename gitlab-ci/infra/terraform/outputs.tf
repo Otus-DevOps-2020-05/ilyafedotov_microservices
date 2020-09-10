7a8ce2386758eb1f7684030593fbafa345518b99
Ilya Fedotov <ilya.v.fedotov@gmail.com>
2020-09-10 23:51:11 +0300

    Added terraform

---
+++ gitlab-ci/infra/terraform/outputs.tf
@@ -0,0 +1,22 @@
+output "inventory" {
+value = <<INVENTORY
+{ "_meta": {
+        "hostvars": {
+    %{ for index, name in yandex_compute_instance.gitlab-ci-vm[*].name ~}
+           "${name}": {
+             "host_name": "${yandex_compute_instance.gitlab-ci-vm[index].name}",
+             "host_ext_ip": "${yandex_compute_instance.gitlab-ci-vm[index].network_interface.0.nat_ip_address}" 
+           },  
+    %{ endfor ~}
+           "dummy": { } 
+        }
+    
+    },
+  "gitlab-ci-vm": { 
+    "hosts": [
+       "${join("\",\"", yandex_compute_instance.gitlab-ci-vm.*.name)}"
+              ]
+  }
+}
+    INVENTORY
+}
